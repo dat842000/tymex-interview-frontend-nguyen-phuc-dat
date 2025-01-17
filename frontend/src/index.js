9d08c3c3448ea8911c838af236cbd24d7375f350
Nguyen Phuc Dat <dnn8420@gmail.com>
2025-01-17 10:32:46 +0700

    chore: add project settings - Install library such as axios antd - Set up folder structure

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DashboardPage from './pages/DashboardPage.tsx';
+import '../src/styles/index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DashboardPage />
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/DashboardPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../components/Header.tsx";
+
+const DashboardPage = () => {
+    return <div className="dashboard">
+        <Header></Header>
+        </div>
+}
+export default DashboardPage;
